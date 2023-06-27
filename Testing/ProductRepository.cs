51cd6e3855fb5bfd8731e12996c6545114de2ef2
Danamarie10 <dana.patrick10@outlook.com>
2023-06-26 23:15:09 -0400

    finished exercise

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -20,5 +20,15 @@ namespace Testing
             return _connection.Query<Product>("SELECT * FROM PRODUCTS");
 
         }
+        public Product GetProduct(int id)
+        {
+            return _connection.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _connection.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+             new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
     }
 }
