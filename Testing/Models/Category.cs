fe0212f75a829eda2342d628cd77ae25178fe963
Danamarie10 <dana.patrick10@outlook.com>
2023-07-07 09:55:06 -0400

    make corrections

--- Testing/Models/Category.cs
+++ Testing/Models/Category.cs
@@ -1,6 +1,9 @@
-﻿namespace Testing.Models
+﻿using System;
+namespace Testing.Models
 {
     public class Category
     {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
     }
 }
